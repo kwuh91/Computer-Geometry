25c80f972a867b0ede1bd69916129ec12fc65cf1
kwuh91 <nkt.ochkin@gmail.com>
2024-10-14 20:01:37 +0300

    LW4.6 done

--- LW4/LW4.6/static/js/6.js
+++ LW4/LW4.6/static/js/6.js
@@ -3,19 +3,44 @@
 "use strict";
 
 class Timer {
+    #secondsImageScaleCoeff = 0.45;
+    #minutesImageScaleCoeff = 0.125;
+    #hoursImageScaleCoeff   = 0.2;
+
+    #secondsImageRelativeCenterOffsetCoeffInit = 0.58;
+    #minutesImageRelativeCenterOffsetCoeffInit = 0.7;
+    #hoursImageRelativeCenterOffsetCoeffInit   = 0.58;
+
+    #imageAbsoluteCenterOffsetTranslate = 1.075;
+
+    #secondsImageRelativeCenterOffsetCoeffTranslate = 0.7325;
+    #minutesImageRelativeCenterOffsetCoeffTranslate = 0.9525;
+    #hoursImageRelativeCenterOffsetCoeffTranslate   = 0.86;
+
+    #one60thOfACircle = 2 * Math.PI / 60;
+
+    #oneSecondInMilliseconds = 1000;
+    #oneMinuteInMilliseconds = this.#oneSecondInMilliseconds * 61;
+    #oneHourInMilliseconds   = this.#oneMinuteInMilliseconds * 61;
+
     constructor(ctx, canvas_width, canvas_height) {
         this.ctx = ctx;
 
         this.canvas_width = canvas_width;
         this.canvas_height = canvas_height;
 
-        this.rotationAngle = 0; // Initialize rotation angle
+        this.secondsRotationAngle = 0;
+        this.minutesRotationAngle = 0;
+        this.hoursRotationAngle   = 0;
+
         this.initialLaunch = true;
-        // this.rotationInterval = null; // To store interval ID
-        this.lastTime = 0;
+
+        this.lastSecondsTime = 0;
+        this.lastMinutesTime = 0;
+        this.lastHoursTime   = 0;
 
         this.initialCenter = {
-            x: canvas_width / 2,
+            x: canvas_width  / 2,
             y: canvas_height / 2        
         };
         
@@ -23,81 +48,90 @@ class Timer {
         this.timerImg = new Image();
         this.timerImg.src = '../../images/stopwatch2.png'
         this.timerCenter = {
-            x: this.initialCenter.x - this.timerImg.naturalWidth/2,
-            y: this.initialCenter.y - this.timerImg.naturalHeight/2
+            x: this.initialCenter.x - this.timerImg.naturalWidth  / 2,
+            y: this.initialCenter.y - this.timerImg.naturalHeight / 2
         }
 
         // seconds
         this.secondsImg = new Image();
         this.secondsImg.src = '../../images/second.png'
-        this.secondsScale = 0.45
+        this.secondsScale = this.#secondsImageScaleCoeff;
         this.secondsSize = {
-            width:  this.secondsImg.naturalWidth * this.secondsScale,
+            width:  this.secondsImg.naturalWidth  * this.secondsScale,
             height: this.secondsImg.naturalHeight * this.secondsScale,
         }
         this.secondsCenter = {
             x: this.initialCenter.x - this.secondsSize.width / 2,
-            y: this.initialCenter.y - this.secondsSize.height * 0.58
+            y: this.initialCenter.y - this.secondsSize.height * this.#secondsImageRelativeCenterOffsetCoeffInit
         }
 
-        this.secondsIsAnimating = false;
+        this.isAnimating = false;
 
         // minutes
         this.minutesImg = new Image();
         this.minutesImg.src = '../../images/minute.png'
-        this.minutesScale = 0.125
+        this.minutesScale = this.#minutesImageScaleCoeff;
         this.minutesSize = {
-            width:  this.minutesImg.naturalWidth * this.minutesScale,
+            width:  this.minutesImg.naturalWidth  * this.minutesScale,
             height: this.minutesImg.naturalHeight * this.minutesScale,
         }
         this.minutesCenter = {
             x: this.initialCenter.x - this.minutesSize.width / 2,
-            y: this.initialCenter.y - this.minutesSize.height * 0.7
+            y: this.initialCenter.y - this.minutesSize.height * this.#minutesImageRelativeCenterOffsetCoeffInit
         }
 
         // hours
         this.hoursImg = new Image();
         this.hoursImg.src = '../../images/hour.png'
-        this.hoursScale = 0.2
+        this.hoursScale = this.#hoursImageScaleCoeff;
         this.hoursSize = {
-            width:  this.hoursImg.naturalWidth * this.hoursScale,
+            width:  this.hoursImg.naturalWidth  * this.hoursScale,
             height: this.hoursImg.naturalHeight * this.hoursScale,
         }
         this.hoursCenter = {
             x: this.initialCenter.x - this.hoursSize.width / 2,
-            y: this.initialCenter.y - this.hoursSize.height * 0.58
+            y: this.initialCenter.y - this.hoursSize.height * this.#hoursImageRelativeCenterOffsetCoeffInit
         }
     }
 
     launchTimer() {
         if (this.initialLaunch) {
             this.initialLaunch = false;
-            this.secondsIsAnimating = true;
+            this.isAnimating = true;
 
             requestAnimationFrame((timestamp) => this.animateRotation(timestamp));
         }
-
-        // Start rotating the image every second
-        // this.rotationInterval = setInterval(() => {
-        //     this.rotationAngle += 2 * Math.PI / 60; // 10 degrees in radians
-        //     this.draw(); // Redraw after updating rotation
-        // }, 1000); // Every second
-
-        // this.rotationAngle += Math.PI / 12; 
-        // this.draw(); 
     }
 
     animateRotation(timestamp) {
-        if (!this.secondsIsAnimating) return; // Stop animation if not needed
+        if (!this.isAnimating) return; // Stop animation if not needed
 
         // Calculate the time difference (in milliseconds)
-        if (!this.lastTime) this.lastTime = timestamp;
-        const elapsedTime = timestamp - this.lastTime;
+        if (!this.lastSecondsTime) this.lastSecondsTime = timestamp;
+        const elapsedSecondsTime = timestamp - this.lastSecondsTime;
+
+        if (!this.lastMinutesTime) this.lastMinutesTime = timestamp;
+        const elapsedMinutesTime = timestamp - this.lastMinutesTime;
+
+        if (!this.lastHoursTime) this.lastHoursTime = timestamp;
+        const elapsedHoursTime = timestamp - this.lastHoursTime;
 
         // Rotate every second (1000 milliseconds)
-        if (elapsedTime >= 1000) {
-            this.rotationAngle += 2 * Math.PI / 60; 
-            this.lastTime = timestamp; // Reset last time
+        if (elapsedSecondsTime >= this.#oneSecondInMilliseconds) {
+            this.secondsRotationAngle += this.#one60thOfACircle;
+            this.lastSecondsTime = timestamp; // Reset last time
+        }
+
+        // Rotate every minute (60000 milliseconds)
+        if (elapsedMinutesTime >= this.#oneMinuteInMilliseconds) {
+            this.minutesRotationAngle += this.#one60thOfACircle;
+            this.lastMinutesTime = timestamp; // Reset last time
+        }
+
+        // Rotate every hour (3600000 milliseconds)
+        if (elapsedHoursTime >= this.#oneHourInMilliseconds) {
+            this.hoursRotationAngle += this.#one60thOfACircle;
+            this.lastHoursTime = timestamp; // Reset last time
         }
 
         // Redraw the scene with the updated rotation
@@ -109,9 +143,6 @@ class Timer {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas_width, this.canvas_height); // Clear canvas
-        // this.ctx.save();
-
-        // this.ctx.translate(this.initialCenter.x, this.initialCenter.y);
 
         // draw timer body
         this.drawTimer();
@@ -120,12 +151,10 @@ class Timer {
         this.drawSeconds();
 
         // draw minutes
-        // this.drawMinutes();
+        this.drawMinutes();
 
-        // // draw hours
-        // this.drawHours();
-
-        // this.ctx.restore();
+        // draw hours
+        this.drawHours();
     }
 
     drawTimer() {
@@ -138,25 +167,13 @@ class Timer {
                                    this.timerImg.naturalHeight);
             };
         } else {
-            // this.ctx.save();
             this.ctx.drawImage(this.timerImg, 
                                this.timerCenter.x, 
                                this.timerCenter.y, 
                                this.timerImg.naturalWidth, 
                                this.timerImg.naturalHeight);
-            // this.ctx.restore();
         }
     }
-
-    // drawSeconds() {
-    //     this.secondsImg.onload = () => {
-    //         this.ctx.drawImage(this.secondsImg, 
-    //                            this.secondsCenter.x, 
-    //                            this.secondsCenter.y, 
-    //                            this.secondsSize.width, 
-    //                            this.secondsSize.height);
-    //     };
-    // }
 
     drawSeconds() {
         if (this.initialLaunch) {
@@ -171,44 +188,75 @@ class Timer {
             // Rotate around the center and draw the image
             this.ctx.save();
 
-            this.ctx.translate(this.initialCenter.x, this.initialCenter.y * 1.075); // actual center offset
+            this.ctx.translate(this.initialCenter.x, this.initialCenter.y * this.#imageAbsoluteCenterOffsetTranslate); // actual center offset
 
-            // this.ctx.fillStyle = 'red';
-            // this.ctx.beginPath();
-            // this.ctx.arc(0, 0, 5, 0, 2 * Math.PI); // (x, y, radius, startAngle, endAngle)
-            // this.ctx.fill();
-
-            this.ctx.rotate(this.rotationAngle); // Apply rotation
+            this.ctx.rotate(this.secondsRotationAngle); // Apply rotation
 
             // Draw the seconds hand, adjusting to draw from its center
             this.ctx.drawImage(this.secondsImg,  
-                            -this.secondsSize.width / 2, 
-                            -this.secondsSize.height * 0.7325, // relative center offset
-                            this.secondsSize.width, 
-                            this.secondsSize.height);
+                              -this.secondsSize.width / 2, 
+                              -this.secondsSize.height * this.#secondsImageRelativeCenterOffsetCoeffTranslate, // relative center offset
+                               this.secondsSize.width, 
+                               this.secondsSize.height);
 
             this.ctx.restore();
         }
     }
 
     drawMinutes() {
-        this.minutesImg.onload = () => {
-            this.ctx.drawImage(this.minutesImg, 
-                               this.minutesCenter.x, 
-                               this.minutesCenter.y, 
+        if (this.initialLaunch) {
+            this.minutesImg.onload = () => {
+                this.ctx.drawImage(this.minutesImg, 
+                                   this.minutesCenter.x, 
+                                   this.minutesCenter.y, 
+                                   this.minutesSize.width, 
+                                   this.minutesSize.height);
+            };
+        } else {
+            // Rotate around the center and draw the image
+            this.ctx.save();
+
+            this.ctx.translate(this.initialCenter.x, this.initialCenter.y * this.#imageAbsoluteCenterOffsetTranslate); // actual center offset
+
+            this.ctx.rotate(this.minutesRotationAngle); // Apply rotation
+
+            // Draw the seconds hand, adjusting to draw from its center
+            this.ctx.drawImage(this.minutesImg,  
+                              -this.minutesSize.width / 2, 
+                              -this.minutesSize.height * this.#minutesImageRelativeCenterOffsetCoeffTranslate, // relative center offset
                                this.minutesSize.width, 
                                this.minutesSize.height);
-        };
+
+            this.ctx.restore();
+        }
     }
 
     drawHours() {
-        this.hoursImg.onload = () => {
-            this.ctx.drawImage(this.hoursImg, 
-                               this.hoursCenter.x, 
-                               this.hoursCenter.y, 
+        if (this.initialLaunch) {
+            this.hoursImg.onload = () => {
+                this.ctx.drawImage(this.hoursImg, 
+                                   this.hoursCenter.x, 
+                                   this.hoursCenter.y, 
+                                   this.hoursSize.width, 
+                                   this.hoursSize.height);
+            };
+        } else {
+            // Rotate around the center and draw the image
+            this.ctx.save();
+
+            this.ctx.translate(this.initialCenter.x, this.initialCenter.y * this.#imageAbsoluteCenterOffsetTranslate); // actual center offset
+
+            this.ctx.rotate(this.hoursRotationAngle); // Apply rotation
+
+            // Draw the seconds hand, adjusting to draw from its center
+            this.ctx.drawImage(this.hoursImg,  
+                              -this.hoursSize.width / 2, 
+                              -this.hoursSize.height * this.#hoursImageRelativeCenterOffsetCoeffTranslate, // relative center offset
                                this.hoursSize.width, 
                                this.hoursSize.height);
-        };
+
+            this.ctx.restore();
+        }
     }
 }   
 
@@ -218,7 +266,7 @@ function main() {
 
     const ctx = canvas.getContext("2d") ;
 
-    canvas.width  = window.innerWidth * 0.99
+    canvas.width  = window.innerWidth  * 0.99
     canvas.height = window.innerHeight * 0.99
 
     const timer = new Timer(ctx, canvas.width, canvas.height);
